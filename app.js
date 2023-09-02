da223e3663acbd4c171d474d39b96a35ed12ae57
timoteo <timoteolbueno@gmail.com>
2023-09-02 05:16:03 -0300

    include of an app.js script, that serves for run a localhost server for the project, and the necessary dependences: nodemon

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('<div><h1>Olá Mundo!</h1></div>')
+})
+
+app.listen(3000, () => {
+    console.log("servidor iniciado")
+})
